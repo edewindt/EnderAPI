5e5fea9a82dc8eed1cfe3f2d2c439a8a87bbcc87
edewindt <edewindt27@outlook.com>
2022-11-20 21:56:16 -0600

    trimmed the fat

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -21,23 +21,11 @@ type Character struct {
 	Quote string
 	Media string
 	Audio string
+	Bio string
+	Trivia string
+	Species string
 }
 
-type Name struct {
-	Name string
-}
-
-type Quote struct {
-	Quote string
-}
-
-type Media struct {
-	Media string
-}
-
-type Audio struct {
-	Audio string
-}
 
 func GetCharData(c *fiber.Ctx) error {
 	var character []*Character
@@ -54,57 +42,3 @@ func GetAllCharData(c *fiber.Ctx) error {
 	pgxscan.Select(ctx, db, &characters, `SELECT * from characters ORDER by id`)
 		return c.JSON(characters)
 }
-
-func GetNames(c *fiber.Ctx) error {
-	var names []*Name
-
-	pgxscan.Select(ctx,db, &names, `SELECT name from characters`)
-	return c.JSON(names)
-}
-
-func GetName(c *fiber.Ctx) error {
-	var name []*Name
-
-	pgxscan.Select(ctx, db, &name, `SELECT name from characters WHERE id = $1`, c.Params("id"))
-	return c.JSON(name[0])
-}
-
-func GetQuote(c *fiber.Ctx) error {
-	var quote []*Quote
-
-	pgxscan.Select(ctx, db, &quote, `SELECT quote from characters WHERE id = $1`, c.Params("id"))
-	return c.JSON(quote[0])
-}
-func GetQuotes(c *fiber.Ctx) error {
-	var quotes []*Quote
-
-	pgxscan.Select(ctx, db, &quotes, `SELECT quote from characters`)
-	return c.JSON(quotes)
-}
-func GetMedia(c *fiber.Ctx) error {
-	var media []*Media
-
-	pgxscan.Select(ctx, db, &media, `SELECT media from characters WHERE id = $1`, c.Params("id"))
-	return c.JSON(media)
-}
-func GetAllMedia(c *fiber.Ctx) error {
-	var medias []*Media
-
-	pgxscan.Select(ctx, db, &medias, `SELECT media from characters`)
-	return c.JSON(medias)
-}
-
-func GetAudio(c *fiber.Ctx) error {
-	var audio []*Audio
-
-	pgxscan.Select(ctx, db, &audio, `SELECT audio from characters WHERE id = $1`, c.Params("id"))
-	return c.JSON(audio)
-}
-
-func GetAllAudio(c *fiber.Ctx) error {
-	var audios []*Audio
-
-	pgxscan.Select(ctx, db, &audios, `SELECT audio from characters`)
-	return c.JSON(audios)
-}
-
--- main.go
+++ main.go
@@ -10,15 +10,6 @@ import (
 func Routes(app *fiber.App) {
 	app.Get("/data", handlers.GetAllCharData)
 	app.Get("/data/:id", handlers.GetCharData)
-	app.Get("/names", handlers.GetNames)
-	app.Get("/names/:id", handlers.GetName)
-	app.Get("/quotes", handlers.GetQuotes)
-	app.Get("/quotes/:id", handlers.GetQuote)
-	app.Get("/media", handlers.GetAllMedia)
-	app.Get("/media/:id", handlers.GetMedia)
-	app.Get("/audio", handlers.GetAllAudio)
-	app.Get("/audio/:id", handlers.GetAudio)
-
 }
 
 func main() {
